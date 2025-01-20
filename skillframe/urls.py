c2d6f700c921e1d45a69a6a46addff2251d5ddd7
cyber-kernel <officialsamcorporation@gmail.com>
2025-01-20 18:04:41 +0530

    implementation of blog

--- skillframe/urls.py
+++ skillframe/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -13,3 +15,7 @@ urlpatterns = [
     # it should be in the last
     path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
